500c5120b13041a7ac595799c28a5b9a25231c1d
Wallington <thenoblebrotherhood@gmail.com>
2018-08-20 20:47:57 -0500

    update the npm lock issue

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,18 @@
+/*
+* Author: Sean O'Brien
+* Create Date: 08/20/2018
+* Modify Date: 08/20/2018
+* Discription: This where site will load our indivaul pages and hold our main template
+*/
+
+import { Component } from '@angular/core';
+
+@Component
+({
+  selector: 'app-root',
+  templateUrl: './main.component.html',
+})
+
+export class MainComponent {
+  title = 'app';
+}
