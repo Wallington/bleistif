0bc95450ca10fb665086e6d2d0474f265cd7079d
Wallington <thenoblebrotherhood@gmail.com>
2018-08-24 16:16:44 -0500

    created the UI for Phone and some of UI for Tablets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
+//importing system components
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
+//google material component module
+import 
+{ 
+  MatButtonModule,
+  MatToolbarModule,
+  MatListModule,
+  MatTabsModule
+} from "@angular/material";
+
+// import any pages components
 import { MainComponent } from './main/main.component';
 
 @NgModule({
-  declarations: [
+  declarations: 
+  [
     MainComponent
   ],
-  imports: [
-    BrowserModule
+  imports: 
+  [
+    BrowserModule,
+    BrowserAnimationsModule, //note any google materail must go after this module follow step 3 note
+    MatButtonModule,
+    MatToolbarModule,
+    MatListModule,
+    MatTabsModule
+  ],
+  exports:
+  [ //also any google materail must also be export as well as import
+    MatButtonModule,
+    MatToolbarModule,
+    MatListModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [MainComponent]
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,7 +1,7 @@
 /*
 * Author: Sean O'Brien
 * Create Date: 08/20/2018
-* Modify Date: 08/20/2018
+* Modify Date: 08/23/2018
 * Discription: This where site will load our indivaul pages and hold our main template
 */
 
@@ -13,6 +13,35 @@ import { Component } from '@angular/core';
   templateUrl: './main.component.html',
 })
 
-export class MainComponent {
-  title = 'app';
+export class MainComponent 
+{
+    links: string[] = ['Feature', 'Graphite','Charcoal','Colored', 'Water Colored', "Accessory", "My Account"];
+    isMenuOpen: boolean = false;
+
+    TogglePhoneMenu()
+    {
+      let mainElem = document.getElementById('main');
+      let navElem = document.getElementById('nav');
+      let iconElem = document.getElementById('navIcon');
+      let navButtonElem = document.getElementById('navButton');
+      this.isMenuOpen = !this.isMenuOpen; //short cut to toggle
+
+      mainElem.className = (this.isMenuOpen) ? "open": "close";
+      mainElem.style.top = (this.isMenuOpen) ? "400px" : "60px";
+
+      navButtonElem.className = "iconRotate mat-icon-button";
+      
+      //set time out for 50 ms so hide the clip of text be see in the opacity main body
+      setTimeout( () =>
+      {
+        navElem.style.opacity = (this.isMenuOpen) ? "1" : "0";
+      },50);
+      setTimeout(() =>
+      {
+          iconElem.className = (this.isMenuOpen) ? "fas fa-times": "fas fa-bars";
+          //reset the button for next rotate
+          navButtonElem.className = "mat-icon-button";
+      }, 200);
+    }
+    
 }
